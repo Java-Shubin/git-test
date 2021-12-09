b67d2f91c54e11c6eb75896f8f848fa45e07abe3
smile <1873665929@qq.com>
2021-12-09 16:29:11 +0800

    IDEA first commit

---
+++ 1_Git-Test/src/main/java/org/smile/GitTest.java
@@ -0,0 +1,12 @@
+package org.smile;
+
+/**
+ * @author smile
+ * @date 2021/12/9
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
