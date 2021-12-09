93b03ac450c63b9e62bd4e0e867cbac5d0e1f7c0
smile <1873665929@qq.com>
2021-12-09 17:16:53 +0800

    IDEA 1 commit by hot-fix

--- 1_Git-Test/src/main/java/org/smile/GitTest.java
+++ 1_Git-Test/src/main/java/org/smile/GitTest.java
@@ -8,5 +8,6 @@ package org.smile;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hot-fix 1");
     }
 }
